f5286e2b03e49637ccb3583ef99a3b161a5ae166
Manuel Sagra de Diego <manuelsagra@gmail.com>
2019-03-20 18:17:59 +0100

    Initial Commit

---
+++ Tie Fighter Assault/AppDelegate.swift
@@ -0,0 +1,48 @@
+//
+//  AppDelegate.swift
+//  Tie Fighter Assault
+//
+//  Created by Manuel Sagra de Diego on 20/03/2019.
+//  Copyright © 2019 Manuel Sagra. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        // Main Window
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = MenuViewController()
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+
+    }
+
+}
+
---
+++ Tie Fighter Assault/Controllers/MenuViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MenuViewController.swift
+//  Tie Fighter Assault
+//
+//  Created by Manuel Sagra de Diego on 20/03/2019.
+//  Copyright © 2019 Manuel Sagra. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
